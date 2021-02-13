d4c4f913ffa0f99ff0b833322de26a30a5d2fd5a
linlinzzo <79007796+linlinzzo@users.noreply.github.com>
2021-02-13 20:54:25 +0800

    Update head.php

--- assets/qblog/head.php
+++ assets/qblog/head.php
@@ -1,11 +1,32 @@
 <?php if(!defined('WMBLOG'))exit;?><!DOCTYPE HTML>
+<html lang="zh">
 <html>
 <head>
+<!-- utf-8 -->
 <meta charset="utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+<!-- Adaptive web page -->
+<meta name="applicable-device"content="pc,mobile">
+<!-- Browser optimization -->
+<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
+<!-- Head key information -->
 <title><?php echo $tit.'_'.$webtitle;?></title>
 <meta name="keywords" content="<?php echo $key;?>" />
 <meta name="description" content="<?php echo $des;?>" />
+<!-- Browser settings -->
+<meta name="x5-orientation" content="portrait">
+<meta name="screen-orientation" content="portrait">
+<meta name="robots" content="index,follow" />
+<meta name="applicable-device"content="pc,mobile">
+<!-- og tag -->
+<meta property="og:type" content="article"/>
+<meta property="og:title" content="<?php echo $tit.'_'.$webtitle;?>">
+<meta property="og:type" content="article"/>
+<meta property="og:site_name" content="<?php echo $webtitle;?>" />
+<meta property="og:description" content="<?php echo $des;?>">
+<meta property="og:url" content="<?php echo $url;?>"> 
+<meta property="article:tag" content="<?php echo $key;?>" />
+<!-- QQ Browser optimization -->
+<meta itemprop="description" content="<?php echo $des;?>">
 <link href="assets/<?php echo TEMPLATE;?>/style.css?v=4.0" rel="stylesheet" type="text/css" />
 <?php if($tpl=='post.php'){?>
 <link href="assets/js/wangeditor/css/wangEditor.min.css" rel="stylesheet" type="text/css" />
@@ -30,8 +51,8 @@
 <div class="other-wrap collapse">
 	  <div class="other box-m">
 	   <div class="desc"><?php echo $motto;?></div>
-	   <form method="get" class="search-form" action="<?php echo $file;?>"> <input class="search-text" name="s" autocomplete="off" placeholder="输入关键词搜索..." required="required" type="text" value="<?php echo $s;?>"> <button class="search-submit" alt="搜索" type="submit">搜索</button></form>
-	   </div>   
+<form method="get" class="search-form" action="<?php echo $file;?>"><input class="search-text" name="s" autocomplete="off" placeholder=" " required="required" type="text" value="<?php echo $s;?>"><button class="search-submit" alt="搜索" type="submit">search</button></form>
+</div>
 </div>
 </div>
 <div id="wrap"> 
